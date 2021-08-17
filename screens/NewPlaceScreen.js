610198a359ac4ca100b9357fa0f9c960f8109bf6
Tanner Schmutte <tannerschmutte@gmail.com>
2021-08-17 12:51:37 -0700

    set up screens and navigation

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+
+const NewPlaceScreen = (props) => {
+    return (
+        <View>
+            <Text>New Place Screen</Text>
+        </View>
+    );
+};
+
+NewPlaceScreen.navigationOptions = {
+    headerTitle: 'Add Place'
+}
+
+const styles = StyleSheet.create({});
+
+export default NewPlaceScreen;
