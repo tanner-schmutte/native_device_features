d40d896a792c081cf4ea530598d014ed387d3cc1
Tanner Schmutte <tannerschmutte@gmail.com>
2021-08-24 21:06:20 -0700

    store new place in the local database

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,4 +1,5 @@
 import * as FileSystem from 'expo-file-system';
+import { insertPlace } from '../../helpers/db';
 
 export const ADD_PLACE = 'ADD_PLACE';
 
@@ -12,13 +13,25 @@ export const addPlace = (title, image) => {
                 from: image,
                 to: newPath,
             });
+
+            const dbResult = await insertPlace(
+                title,
+                newPath,
+                'Dummy address',
+                15.6,
+                12.3
+            );
+
+            dispatch({
+                type: ADD_PLACE,
+                placeData: {
+                    id: dbResult.insertId,
+                    title: title,
+                    image: newPath,
+                },
+            });
         } catch (err) {
             throw err;
         }
-
-        dispatch({
-            type: ADD_PLACE,
-            placeData: { title: title, image: newPath },
-        });
     };
 };
