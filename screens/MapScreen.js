2075a7bcae39a9004bbf905fa4bd872e67e7bb71
Tanner Schmutte <tannerschmutte@gmail.com>
2021-08-28 14:59:28 -0700

    display interactive map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = (props) => {
-    return (
-        <View>
-            <Text>Map Screen</Text>
-        </View>
-    );
+    const mapRegion = {
+        latitude: 37.78,
+        longitude: -122.43,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    };
+    return <MapView region={mapRegion} style={styles.map} />;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    map: {
+        flex: 1,
+    },
+});
 
 export default MapScreen;
